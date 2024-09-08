71089765ea939e9f87b55c4ed9769cffbcc64ee4
Sac <104184816+gitayam@users.noreply.github.com>
2024-09-08 19:18:45 -0400

    fixing imports

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import base64
 
 load_dotenv()
 
@@ -10,6 +11,9 @@ AUTHENTIK_API_URL = os.getenv("AUTHENTIK_API_URL")
 MAIN_GROUP_ID = os.getenv("MAIN_GROUP_ID")
 BASE_DOMAIN = os.getenv("BASE_DOMAIN")
 FLOW_ID = os.getenv("FLOW_ID")
-ENCRYPTION_PASSWORD = base64.urlsafe_b64encode(hashlib.sha256(os.getenv("ENCRYPTION_PASSWORD").encode()).digest())
 SHLINK_API_TOKEN = os.getenv("SHLINK_API_TOKEN")
 SHLINK_URL = os.getenv("SHLINK_URL")
+ENCRYPTION_PASSWORD_RAW = os.getenv("ENCRYPTION_PASSWORD")
+if not ENCRYPTION_PASSWORD_RAW:
+    raise ValueError("ENCRYPTION_PASSWORD is not set in the environment variables")
+ENCRYPTION_PASSWORD = base64.urlsafe_b64encode(hashlib.sha256(ENCRYPTION_PASSWORD_RAW.encode()).digest())
